47f0df0ddfcae5e34f5d580a8bef6e07e8b3269b
Maxim Bogachew <max.bogachew@yandex.ru>
2023-08-10 18:35:13 +0400

    Написал метод для добавления пользователя

--- Models/User.php
+++ Models/User.php
@@ -3,13 +3,27 @@ namespace Models;
 
 class User{
     public function getUser($id){
-        $users = json_decode(file_get_contents($_SERVER["DOCUMENT_ROOT"]."users.json"), JSON_OBJECT_AS_ARRAY);
+        $users = $this->getUsers();
         $user = NULL;
         foreach($users as $u){
             if($u["id"] == $id) $user = $u;
             continue;
         }
         return $user;
+    }
+
+    public function pushUser($user){
+        $users = $this->getUsers();
+        $users[] = $user;
+        $this->write();
+    }
+
+    private function getUsers(){
+        return json_decode(file_get_contents($_SERVER["DOCUMENT_ROOT"]."/users.json"), JSON_OBJECT_AS_ARRAY);
+    }
+
+    private function write($arUsers){
+        file_put_contents($_SERVER["DOCUMENT_ROOT"]."/users.json", json_encode($arUsers, JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
     }
 }
 ?>
